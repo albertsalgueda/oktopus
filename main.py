f4027a63f962e869d1b9f9fc97c7360db754e919
Albert Salgueda <salguedainsti@gmail.com>
2022-02-14 13:41:16 -0500

    classes definition

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 
 class Campaign():
-    def __init__(self,id,budget,spent,conversions,roi):
+
+    def __init__(self,id,budget,spent,impressions,conversions,roi):
+        #falta determinar como podemos saber el tiempo que lleva la campaña 
         self.id = id    
         self.budget = budget
         self.spent = spent
+        self.impressions = impressions
         self.conversions = conversions
         self.roi = roi
 
+    def change_budget(self,increment):
+        #increment debe ser un valor numerico para editar el ( daily budget )
+        self.budget = self.budget + increment
+    
 
 class State(Campaign):
 
@@ -14,4 +21,9 @@ class State(Campaign):
         self.budget = budget
         self.time = time
         self.campaigns = campaigns
-    
+        self.budget_allocation = {}
+
+    def get_budget_allocation(self):
+        #returns a dictionary with the budget allocation
+        for campaign in campaigns:
+
