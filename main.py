c98938ea4d81998c482700d25bb1db48d3c4b877
Albert Salgueda <salguedainsti@gmail.com>
2022-02-20 19:57:10 -0500

    simulation system

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ class Campaign():
         self.conversions = conversions
         self.roi = roi
 
+    def update(self,impressions,conversions,roi):
+        self.spent += self.budget
+        self.impressions += int(impressions)
+        self.conversions += int(conversions)
+        self.roi = float(roi)
+
     def change_budget(self,increment):
         #increment debe ser un valor numerico para editar el ( daily budget )
         self.budget = self.budget + increment
--- simulation.py
+++ simulation.py
@@ -5,6 +5,9 @@ def budget_printer(campaign_group):
     budget = campaign_group.get_state()
     for campaign in budget:
         print(f'{campaign} has a {round(budget[campaign]*100)}% of the current budget, which is {campaign_group.campaigns[i].budget}')
+        print(f'{campaign} has {campaign_group.campaigns[i].impressions} impressions')
+        print(f'{campaign} has {campaign_group.campaigns[i].conversions} conversions')
+        print(f'{campaign} has {campaign_group.campaigns[i].roi} ROI')
         i +=1
 
 print('Welcome to the simulation of Oktopus ;)')
@@ -20,11 +23,17 @@ budget = int(input('Introduce total budget: '))
 time = int(input('Introduce the number of timestamps: '))
 campaign_group = State(budget,time,campaigns)
 while campaign_group.remaining > 0:
+    print('###########################################')
     print(f'Budget at timestamp {campaign_group.current_time} is {campaign_group.current_budget}')
     budget_printer(campaign_group)
     print(f'Remaining budget:{campaign_group.next_timestamp()}')
     print(f'Available actions at timestamp {campaign_group.current_time}')
     #show the list of available actions
     action = input("Take an action, press enter for no action")
+    for campaign in campaign_group.campaigns:
+        print(f'Introduce new data for campaign {campaign.id}')
+        data = str(input("new Impresions, new Conversions and new ROI respectively separated with a comma: "))
+        data = data.split(',')
+        campaign.update(data[0],data[1],data[2])
 
 print('Optimization finished, thanks for trusting Oktopus')
